1ef6632ca6e8a226a234a4fb8a720eeb811ce091
Joseph Utecht <thenmal@gmail.com>
2014-03-11 16:53:14 -0500

    tiled plugin basically working

--- Assets/TiledEditor.cs
+++ Assets/TiledEditor.cs
@@ -7,8 +7,6 @@ using System.Xml;
 public class TiledEditor : EditorWindow {
 	string current_level;
 	bool groupEnabled;
-	bool myBool = true;
-	float myFloat = 1.23f;
 
 	private XmlDocument doc;
 	private GameObject base_sprite;
@@ -28,6 +26,7 @@ public class TiledEditor : EditorWindow {
 	void add_tile(int x, int y, int gid){
 		if(gid != 0){
 			GameObject g = (GameObject)Instantiate(base_sprite);
+			g.name = x + "_" + y;
 			g.transform.parent = transform;
 			g.transform.position = new Vector3(transform.position.x + x, transform.position.y - y, 0.0f);
 			SpriteRenderer sr = g.GetComponent<SpriteRenderer>();
@@ -35,9 +34,10 @@ public class TiledEditor : EditorWindow {
 		}
 	}
 	
-	void add_object(float x, float y, int gid){
+	void add_object(float x, float y, int gid, string name){
 		if(gid != 0){
 			GameObject g = (GameObject)Instantiate(object_sprite);
+			g.name = name;
 			g.transform.position = new Vector3(transform.position.x + x, transform.position.y - y, 0.0f);
 			SpriteRenderer sr = g.GetComponent<SpriteRenderer>();
 			sr.sprite = sprites[gid];
@@ -63,7 +63,11 @@ public class TiledEditor : EditorWindow {
 		foreach(XmlNode child in node.ChildNodes){
 			float x = int.Parse(child.Attributes["x"].Value) / 32.0f;
 			float y = (int.Parse(child.Attributes["y"].Value) - 32) / 32.0f;
-			add_object(x, y, int.Parse(child.Attributes["gid"].Value));
+			string name = "tiled_object";
+			try{
+				name = child.Attributes["name"].Value;
+			} catch {}
+			add_object(x, y, int.Parse(child.Attributes["gid"].Value), name);
 		}
 	}
 
@@ -102,6 +106,17 @@ public class TiledEditor : EditorWindow {
 		}
 	}
 
+	void CleanUp(){
+		doc = null;
+		transform = null;
+		sprites.Clear();
+		current_level = "";
+		//object_sprite = null;
+		//base_sprite = null;
+		DestroyImmediate(object_sprite);
+		DestroyImmediate(base_sprite);
+	}
+
 	void OnGUI()
 	{
 		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
@@ -110,15 +125,17 @@ public class TiledEditor : EditorWindow {
 			OpenLevel();
 		}
 		transform = (Transform)EditorGUILayout.ObjectField(transform, typeof(Transform), true);
-		if(GUILayout.Button("Create Background", GUILayout.Width(200))){
-			DrawBackground();
-		}
-		if(GUILayout.Button("Create Objects", GUILayout.Width(200))){
-			DrawObjects();
-		}
-		groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
-			myBool = EditorGUILayout.Toggle ("Toggle", myBool);
-			myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+		groupEnabled = (transform != null && doc != null);
+		groupEnabled = EditorGUILayout.BeginToggleGroup ("Once Loaded", groupEnabled);
+			if(GUILayout.Button("Create Background", GUILayout.Width(200))){
+				DrawBackground();
+			}
+			if(GUILayout.Button("Create Objects", GUILayout.Width(200))){
+				DrawObjects();
+			}
 		EditorGUILayout.EndToggleGroup ();
+		if(GUILayout.Button("Clean Up", GUILayout.Width(200))){
+			CleanUp();
+		}
 	}
 }
