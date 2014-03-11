f9507318b992729a9360f4600ca8ff3e55391c38
Joseph Utecht <thenmal@gmail.com>
2014-03-11 16:20:37 -0500

    parenting working

--- Assets/TiledEditor.cs
+++ Assets/TiledEditor.cs
@@ -5,23 +5,21 @@ using System.Collections.Generic;
 using System.Xml;
 
 public class TiledEditor : EditorWindow {
-	public string level_name;
+	string current_level;
+	bool groupEnabled;
+	bool myBool = true;
+	float myFloat = 1.23f;
+
 	private XmlDocument doc;
-	public Texture2D[] textures;
 	private GameObject base_sprite;
 	private GameObject object_sprite;
+	Transform transform;
 	private List<Sprite> sprites;
-	public GameObject base_node;
-
-	void print_children(XmlNode node){
-		foreach(XmlNode child in node){
-			Debug.Log(child.Value);
-		}
-	}
 
 	void load_sprites(string filename){
 		Debug.Log("Loading: " + filename);
 		Sprite[] ss = Resources.LoadAll<Sprite>(filename);
+		sprites.Add(null);
 		foreach(Sprite s in ss){
 			sprites.Add(s);
 		}
@@ -30,22 +28,23 @@ public class TiledEditor : EditorWindow {
 	void add_tile(int x, int y, int gid){
 		if(gid != 0){
 			GameObject g = (GameObject)Instantiate(base_sprite);
-			g.transform.position = new Vector3(base_node.transform.position.x + x, base_node.transform.position.y - y, 0.0f);
+			g.transform.parent = transform;
+			g.transform.position = new Vector3(transform.position.x + x, transform.position.y - y, 0.0f);
 			SpriteRenderer sr = g.GetComponent<SpriteRenderer>();
-			sr.sprite = sprites[gid-2];
+			sr.sprite = sprites[gid];
 		}
 	}
-
+	
 	void add_object(float x, float y, int gid){
 		if(gid != 0){
 			GameObject g = (GameObject)Instantiate(object_sprite);
-			g.transform.position = new Vector3(base_node.transform.position.x + x, base_node.transform.position.y - y, 0.0f);
+			g.transform.position = new Vector3(transform.position.x + x, transform.position.y - y, 0.0f);
 			SpriteRenderer sr = g.GetComponent<SpriteRenderer>();
-			sr.sprite = sprites[gid-2];
+			sr.sprite = sprites[gid];
 			sr.sortingOrder = 1;
 		}
 	}
-
+	
 	void draw_tiles(XmlNode x){
 		Debug.Log("Drawing tiles");
 		Debug.Log("width: " + x.Attributes["width"].Value + " height: " + x.Attributes["height"].Value);
@@ -58,7 +57,7 @@ public class TiledEditor : EditorWindow {
 			i++;
 		}
 	}
-
+	
 	void draw_objects(XmlNode node){
 		Debug.Log("Drawing Objects");
 		foreach(XmlNode child in node.ChildNodes){
@@ -68,39 +67,58 @@ public class TiledEditor : EditorWindow {
 		}
 	}
 
-	// Use this for initialization
-	void nothing () {
+
+	// Add menu item named "My Window" to the Window menu
+	[MenuItem("Window/Tiled Editor")]
+	public static void ShowWindow()
+	{
+		//Show existing window instance. If one doesn't exist, make one.
+		EditorWindow.GetWindow(typeof(TiledEditor));
+	}
+
+	void OpenLevel(){
+		current_level = EditorUtility.OpenFilePanel("Open level file", Application.dataPath, "tmx;xml");
 		base_sprite = new GameObject("tile");
 		base_sprite.AddComponent("SpriteRenderer");
 		object_sprite = new GameObject("object");
 		object_sprite.AddComponent("SpriteRenderer");
 		doc = new XmlDocument();
-		doc.Load(Application.dataPath + "/Resources/" + level_name);
+		doc.Load(current_level);
 		sprites = new List<Sprite>();
 		foreach(XmlNode node in doc.GetElementsByTagName("tileset")){
 			load_sprites(node.Attributes["name"].Value);
 		}
+	}
+
+	void DrawBackground(){
 		foreach(XmlNode node in doc.GetElementsByTagName("layer")){
 			draw_tiles(node);
 		}
+	}
+
+	void DrawObjects(){
 		foreach(XmlNode node in doc.GetElementsByTagName("objectgroup")){
 			draw_objects(node);
 		}
 	}
 
-	[MenuItem ("Window/Tiled Editor")]
-	void Init(){
-		TiledEditor window = (TiledEditor)EditorWindow.GetWindow (typeof (TiledEditor));
-	}
-	
-	// Update is called once per frame
-	void OnGUI () {
+	void OnGUI()
+	{
 		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
-		level_name = EditorGUILayout.TextField ("Text Field", level_name);
-		
-		//groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
-		//myBool = EditorGUILayout.Toggle ("Toggle", myBool);
-		//myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+		current_level = EditorGUILayout.TextField ("Current Level", current_level);
+		if(GUILayout.Button("Open Tiled File", GUILayout.Width(200))){
+			OpenLevel();
+		}
+		transform = (Transform)EditorGUILayout.ObjectField(transform, typeof(Transform), true);
+		if(GUILayout.Button("Create Background", GUILayout.Width(200))){
+			DrawBackground();
+		}
+		if(GUILayout.Button("Create Objects", GUILayout.Width(200))){
+			DrawObjects();
+		}
+		groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
+			myBool = EditorGUILayout.Toggle ("Toggle", myBool);
+			myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
 		EditorGUILayout.EndToggleGroup ();
 	}
 }
